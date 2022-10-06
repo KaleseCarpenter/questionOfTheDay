7faaf08e394608c330342efc0c06ae9a699c0830
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-10-06 13:58:56 -0400

    Added Slice

--- questions/main.go
+++ questions/main.go
@@ -12,6 +12,15 @@ type question struct {
 }
 
 // Slice or Map
+var questions = []question{
+	{ID: "1", Question: "What''s something you really resent paying for?", Author: "Kalese Carpenter"},
+	{ID: "2", Question: "If you couldn''t be convicted of any one type of crime, what criminal charge would you like to be immune to?", Author: "Mandee Rizzi"},
+	{ID: "3", Question: "In the past people were buried with the items they would need in the afterlife, what would you want buried with you so you could use it in the afterlife?", Author: "Zack Raney"},
+	{ID: "4", Question: "Who do you go out of your way to be nice to?", Author: "John Butcher"},
+	{ID: "5", Question: "What invention doesn''t get a lot of love, but has greatly improved the world?", Author: "Jessie Gorrell"},
+	{ID: "6", Question: "What movie quotes do you use on a regular basis?", Author: "Eric Boggs"},
+	{ID: "7", Question: "What “old person” things do you do?", Author: "Cha''Diamond Moody"},
+}
 
 // Get All Questions
 // func getAllQuestions() {
